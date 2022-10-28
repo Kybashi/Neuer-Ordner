fbd8e34b21b754a8b8c19d0b6e00a21cd1541921
xZeyrunner <zeyrunner@gmail.com>
2022-10-28 14:04:05 +0200

    html navbar (dropdown& submit)

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
+from contextlib import redirect_stderr, redirect_stdout
 from flask import Flask, render_template, request
 from DbContext import create_connection
 
 database = r"C:\DB\gpx\gpx.db"
 
 app = Flask(__name__)
-
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    conn = create_connection(database)
-    fahrt = ('testname',1,2)
+@app.route("/home")
+def hello():
+    return render_template("home.html")
 
-    return "Hello, Flask!"
